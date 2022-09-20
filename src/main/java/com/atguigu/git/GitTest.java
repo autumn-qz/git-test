87436f2365415e499314f49579b885fefa28821f
qz <qz@163.com>
2022-09-20 15:59:10 +0800

    h2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git");
+        System.out.println("hot");
     }
 }
