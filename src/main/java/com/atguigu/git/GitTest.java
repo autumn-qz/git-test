1cc1aae86e0068ca441edd033f300788298d1169
qz <qz@163.com>
2022-09-20 16:49:31 +0800

    qawd

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("master");
         System.out.println("hot");
+        System.out.println("hot");
     }
 }
